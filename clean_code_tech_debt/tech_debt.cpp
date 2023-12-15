c9645c2c2edfdbe3cff335d2d0ab899d39590ff2
Camilo Alejandro <cacastilloben@unal.edu.co>
2023-12-15 16:18:20 -0500

    Choose suitable variable names

--- clean_code_tech_debt/tech_debt.cpp
+++ clean_code_tech_debt/tech_debt.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
 
-int main
+int main()
+{
+    std::cout << "\n\tTech Debt - Clean Code\n\n\t";
+    
+    std::cout << "Exercise: choose suitable variable names.\n\t";
+    std::cout << "const today = new Date()\n\t";
+    std::cout << "const elapsed_days: number = 23\n\t";
+    std::cout << "const files_in_directory = 33\n\t";
+    std::cout << "const first_name = Camilo\n\t";
+    std::cout << "const last_name = Castillo\n\t";
+    std::cout << "const last_day_of_modification = 12\n\t";
+    std::cout << "const max_classes_per_student = 6\n\n";
 
+    return 0;
+}
