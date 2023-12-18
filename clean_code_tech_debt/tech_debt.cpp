8ac7f567181bc44e865ce713418194a933b37e71
Camilo Alejandro <cacastilloben@unal.edu.co>
2023-12-18 15:55:41 -0500

    Choose suitable names according to the type

--- clean_code_tech_debt/tech_debt.cpp
+++ clean_code_tech_debt/tech_debt.cpp
@@ -13,5 +13,27 @@ int main()
     std::cout << "const last_day_of_modification = 12\n\t";
     std::cout << "const max_classes_per_student = 6\n\n";
 
+//------------------------------------------------------------------------
+
+    std::cout << "\n\tExercise: choose suitable names according to the type.\n\n\t";
+    std::cout << "const celsius_temps = [33.6, 12.34]\n\t";
+    std::cout << "const server_ip = 123.123.123.123\n\t";
+    std::cout << "const users = [{id: 1, email: 'camilo@gmail.com'}, {id: 2, email: 'lina@gmail.com'}]\n\t";
+    std::cout << "const user_emails = users.map( user => user.email )\n\t";
+    std::cout << "can_jumping = false\n\t";
+    std::cout << "can_running = true\n\t";
+    std::cout << "has_items = false\n\t";
+    std::cout << "is_loading = false\n\t";
+    std::cout << "initial_time = new Date().getTime()\n\t";
+    std::cout << "time_elapsed = new Date().getTime() - initial_time\n\t";
+    std::cout << "function get_books()\n\t";
+    std::cout << "function get_books_by_url()\n\t";
+    std::cout << "function calculate_square_area(float side)\n\t";
+    std::cout << "function print_job()\n\t";
+
+//------------------------------------------------------------------------
+
+//------------------------------------------------------------------------
+
     return 0;
 }
