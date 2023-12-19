a19cde648252063f81d931ae748eba62b996e3f6
Camilo Alejandro <cacastilloben@unal.edu.co>
2023-12-19 17:46:11 -0500

    Refactor functions

--- clean_code_tech_debt/tech_debt.cpp
+++ clean_code_tech_debt/tech_debt.cpp
@@ -13,7 +13,7 @@ int main()
     std::cout << "const last_day_of_modification = 12\n\t";
     std::cout << "const max_classes_per_student = 6\n\n";
 
-//-------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
     std::cout << "\n\tExercise: choose suitable names according to the type.\n\n\t";
     std::cout << "const celsius_temps = [33.6, 12.34]\n\t";
@@ -31,7 +31,7 @@ int main()
     std::cout << "function calculate_square_area(float side)\n\t";
     std::cout << "function print_job()\n\t";
 
-//-------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
     std::cout << "\n\tExercise: choose suitable names for the functions.\n\n\t";
     std::cout << "function get_movie_by_id(id: string)\n\t";
@@ -41,7 +41,69 @@ int main()
     std::cout << "function create_movie(new_movie: movie{})\n\t";
     std::cout << "function create_actor(fullname: string, birthdate: Date)\n\t";
 
-//-------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+    /*
+    (() => {
+
+
+    // Resolver sin la triple condicional dentro del if
+    // includes? arrays?
+    function isRedFruit( fruit: string ): boolean {
+
+        const redFruits = ['manzana','cereza','ciruela'];
+        return redFruits.includes(fruit))
+    }
+
+    // Simplificar esta función
+    // switch? Object literal? validar posibles colores
+    function getFruitsByColor( color: string ): string[] {
+
+        switch(color)
+        {
+            case 'red':    return ['manzana','fresa'];
+            case 'yellow': return ['piña', 'banana'];
+            case 'purple': return ['moras','uvas']
+            default:
+                throw Error('the color must be: red, yellow, purple');
+        }
+    }
+
+    // Simplificar esta función
+    let isFirstStepWorking  = true;
+    let isSecondStepWorking = true;
+    let isThirdStepWorking  = true;
+    let isFourthStepWorking = true;
+
+    function workingSteps() {
+
+        if( ~isFirstStepWorking)    return 'First step broken.';
+        if( ~isSecondStepWorking )  return 'Second step broken.';
+        if( ~isThirdStepWorking )   return 'Third step broken.';
+        if( ~isFourthStepWorking )  return 'Fourth step broken.';
+
+        return 'Working properly!';
+    }
+
+
+    // isRedFruit
+    console.log({ isRedFruit: isRedFruit('cereza'), fruit: 'cereza' }); // true
+    console.log({ isRedFruit: isRedFruit('piña'), fruit: 'piña' }); // true
+
+    //getFruitsByColor
+    console.log({ redFruits: getFruitsByColor('red') }); // ['manzana', 'fresa']
+    console.log({ yellowFruits: getFruitsByColor('yellow') }); // ['piña', 'banana']
+    console.log({ purpleFruits: getFruitsByColor('purple') }); // ['moras', 'uvas']
+    // console.log({ pinkFruits: getFruitsByColor('pink') }); // Error: the color must be: red, yellow, purple
+
+    // workingSteps
+    console.log({ workingSteps: workingSteps() }); // Cambiar los valores de la línea 31 y esperar los resultados
+
+
+    })();
+    */
+//------------------------------------------------------------------------------
 
     return 0;
+
 }
