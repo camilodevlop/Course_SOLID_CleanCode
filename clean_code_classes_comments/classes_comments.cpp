9995fb68b77c9a591a117764be238496a7502e0b
Camilo Alejandro <cacastilloben@unal.edu.co>
2023-12-21 12:00:46 -0500

    Observe problematic inheritance

--- clean_code_classes_comments/classes_comments.cpp
+++ clean_code_classes_comments/classes_comments.cpp
@@ -6,7 +6,6 @@ enum Gender{ MALE, FEMALE};
 
 class Person
 {
-
     public:
         string mName;
         Gender mGender;
@@ -20,8 +19,6 @@ class Person
         ~Person() {};
 };
 
-//------------------------------------------------------------------------------
-
 void Person::printPerson() const
 {
     std::cout << "\nPerson:"
@@ -33,10 +30,47 @@ void Person::printPerson() const
 
 //------------------------------------------------------------------------------
 
+class User: public Person
+{
+    public:
+        string mUsername;
+        string mPassword;
+
+        User(string username = string(), string password = string(), Person person = Person()):
+            mUsername{username}, mPassword{password} {}
+
+        bool checkCredentials() const { return true; }
+
+        ~User() {};
+};
+
+//------------------------------------------------------------------------------
+
+class UserSettings: public User
+{
+    public:
+        string mWorkingDirectory;
+        string mLastOpenedFolder;
+
+        UserSettings(string workingDirectory = string(), string lastOpenFolder = string(), User user = User()):
+            mWorkingDirectory{workingDirectory}, mLastOpenedFolder{lastOpenFolder}{}
+
+        ~UserSettings() {};
+};
+
+//------------------------------------------------------------------------------
+
 int main()
 {
     Person person("Camilo", MALE, "01/01/1990");
     person.printPerson();
+
+    User username("CamiloA", "1234");
+
+    UserSettings userSettings("C:/Users/CamiloA", "C:/Users/CamiloA/Downloads", username);
+
+    userSettings.mBirthday = "01/01/1990";
+    cout << "\n\n" << userSettings.mBirthday << endl;
 
     return 0;
 }
